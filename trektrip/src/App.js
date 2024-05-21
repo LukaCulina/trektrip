2ffe159d8eaf6d854aacba816bfdf1ed031ac62a
Luka Čulina <luka.culina56@gmail.com>
2024-05-21 17:05:18 +0200

    Added pages and components

--- trektrip/src/App.js
+++ trektrip/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <AuthContextProvider>
+        <Header></Header>
+        <div className="app">
+          <Container maxWidth="xl">
+                <Routes>
+                  <Route path='/' element={<Home/>} exact/>
+                  <Route path='/profil' element={<User/>}/>
+                  <Route path='/putovanja' element={<Trips/>}/>
+                  <Route path='/prijava' element={<Login/>}/>
+                </Routes>
+          </Container>
+        </div>
+        <Footer></Footer>
+      </AuthContextProvider>
+  </HashRouter>
   );
 }
 
